9adba4881d4a3171852c2423cfe508b7f4c2151f
Michele Sifanno <michelesifanno@Air-di-Michele.homenet.telecomitalia.it>
2024-04-18 15:05:45 +0200

    video home

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,48 +1,77 @@
 import { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 import { useTheme } from '@mui/material/styles';
-import { Typography, Grid, Container, Card, CardContent, CardMedia } from '@mui/material';
-
+import { Container } from '@mui/material';
 
 export default function Homepage() {
+  const theme = useTheme();
 
-    const theme = useTheme();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, []);
+  const [activeComponent, setActiveComponent] = useState('calendar');
 
-    const [activeComponent, setActiveComponent] = useState('calendar');
+  const handleMenuItemClick = (menuItem) => {
+    setActiveMenuItem(menuItem);
+  };
 
-    const handleMenuItemClick = (menuItem) => {
-        setActiveMenuItem(menuItem);
-    };
-
-    return (
-        <>
-            <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="video"
-        autoPlay
-        loop
-        muted
-        image="https://assets.codepen.io/6093409/river.jpg"
-        style={{ height: '200px' }} // Altezza del video
+  return (
+    <>
+      <Container
+        maxWidth="false"
+        disableGutters
+        sx={{
+          marginTop: '-80px',
+          minWidth: '100vw',
+          position: 'relative', // Aggiunto posizione relativa al container
+        }}
       >
-        <source
-          src="https://assets.codepen.io/6093409/river.mp4"
-          type="video/mp4"
-        />
-      </CardMedia>
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Video Title
+        <Typography
+          variant="h1"
+          component="h1"
+          sx={{
+            position: 'absolute', // Posizionamento assoluto rispetto al container
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)', // Centra il testo orizzontalmente e verticalmente
+            zIndex: 1, // Assicura che il testo sia sopra al video
+            color: 'white', // Colore del testo
+            textAlign: 'center', // Allinea il testo al centro
+          }}
+        >
+          Haxzone<br/>
+          League
         </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Some description about the video.
-        </Typography>
-      </CardContent>
-    </Card>
-        </>
-    );
+        <Box
+          sx={{
+            position: 'relative',
+            width: '100%',
+            paddingTop: '56.25%',
+            backgroundImage: `url('https://img.youtube.com/vi/JkAptaaFSrE/maxresdefault.jpg')`,
+            backgroundSize: 'cover',
+            overflowX: 'hidden',
+          }}
+        >
+          <iframe
+            title="video-background"
+            width="100%"
+            height="100%"
+            src="https://www.youtube.com/embed/JkAptaaFSrE?mute=1&autoplay=1&loop=1&controls=0&showinfo=0&autohide=0&enablejsapi=1&modestbranding=1&playlist=JkAptaaFSrE&vq=hd1080"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              pointerEvents: 'none',
+            }}
+          ></iframe>
+        </Box>
+      </Container>
+    </>
+  );
 }
